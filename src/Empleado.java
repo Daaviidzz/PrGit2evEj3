b5694fc79546960684fec7aad59658933a54d125
Daaviidzz <dballestero366@gmail.com>
2025-03-19 14:21:15 +0100

    Ver1 de rama1

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,9 @@
 public class Empleado {
+
+    void incrementarSueldo(int porcentajeincremento){
+        this.sueldo += sueldo *(1+porcentajeincremento/100.0);
+    }
+
     private String nombre;
     private int edad;
     private double sueldo;
